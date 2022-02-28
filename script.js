786b35ba81d011320efde313bca7da5bf97cd488
Kwame Agyemang <agyemangyeb@gmailcom>
2022-02-28 13:40:23 -0500

    commit in progress

--- script.js
+++ script.js
@@ -11,14 +11,15 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-// generate rendom number 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+// generate random number 
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
+
+
 // when there is no imput
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No number!';
@@ -26,12 +27,11 @@ document.querySelector('.check').addEventListener('click', function () {
 // when player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 correct Number!';
-
-// changing background color when user wins
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
-
-// changing size of number
     document.querySelector('.number').style.width = '30rem'
+
+
 
 // when guess is too high
   } else if (guess > secretNumber) {
@@ -56,3 +56,14 @@ document.querySelector('.check').addEventListener('click', function () {
       }
   }
 });
+
+document.querySelector('.again').addEventListener('click', function () {
+    score = 20;
+    let secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.message').textContent = 'Start guesing...'
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+ })
