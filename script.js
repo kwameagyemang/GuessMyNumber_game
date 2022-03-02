32556eb897432228424581d5da8791617815ffb7
Kwame Agyemang <agyemangyeb@gmailcom>
2022-03-02 16:32:40 -0500

    commit in progress

--- script.js
+++ script.js
@@ -14,6 +14,11 @@ console.log(document.querySelector('.guess').value);
 // generate random number 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+    document.querySelector('.message').textContent = message;
+};
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -22,19 +27,39 @@ document.querySelector('.check').addEventListener('click', function () {
 
 // when there is no imput
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔️ No number!';
+    // document.querySelector('.message').textContent = '⛔️ No number!';
+    displayMessage('⛔️ No number!')
 
 // when player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 correct Number!';
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('.number').style.width = '30rem'
+    document.querySelector('.number').style.width = '30rem';
 
+    if (score > highScore) {
+        highScore = score;
+        document.querySelector('.highScore').textContent = highScore;
+    };
 
-
-// when guess is too high
-  } else if (guess > secretNumber) {
+    
+    // DRY CODE
+    // when guess is wrong
+} else if (guess !== secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 
+      guess > secretNumber? '📈 Too high!' : '📉Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+}
+   
+    /*------------------------C0DE NOT DRY-----------------------
+    // when guess is too high
+  }else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too high!';
       score--;
@@ -55,7 +80,9 @@ document.querySelector('.check').addEventListener('click', function () {
         document.querySelector('.score').textContent = 0;
       }
   }
+  ---------------------------------------------------------------------------*/
 });
+
 
 document.querySelector('.again').addEventListener('click', function () {
     score = 20;
@@ -66,4 +93,4 @@ document.querySelector('.again').addEventListener('click', function () {
     document.querySelector('.guess').value = '';
     document.querySelector('body').style.backgroundColor = '#222';
     document.querySelector('.number').style.width = '15rem';
- })
+ });
